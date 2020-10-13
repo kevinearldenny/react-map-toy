c655399bb4dadd0609ab1409dc3cd74506f8bf90
kevinearldenny <kdenny@azavea.com>
2020-10-13 15:19:21 -0400

    Start refactor into functional components

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,33 +1,40 @@
-import React from "react";
-import '../index.css';
-import * as L from 'leaflet'
+import React, { useEffect, useRef } from "react";
+import "../index.css";
+import * as L from "leaflet";
 
+const Map = (props) => {
+  const myMap = useRef(false);
+  useEffect(() => {
+    const myMap = L.map("mapid");
+    L.tileLayer(
+      "http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png",
+      {
+        attribution:
+          'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        maxZoom: 18,
+      }
+    ).addTo(myMap);
+  }, []);
 
-export default class Map extends React.Component {
+  useEffect(() => {
+    myMap.setView([props.location.latitude, props.location.longitude], 13);
+    const circle = L.circle(
+      [props.location.latitude, props.location.longitude],
+      {
+        color: "blue",
+        fillColor: "#f03",
+        fillOpacity: 0.5,
+        radius: 100,
+      }
+    ).addTo(myMap);
+    circle.bindPopup("Hey, that's me!");
+  }, [props.location.latitude, props.location.longitude]);
 
-    componentDidMount() {
-        const myMap = L.map('mapid')
-        myMap.setView([this.props.location.latitude, this.props.location.longitude], 13);
-        L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}.png', {
-            attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            maxZoom: 18}).addTo(myMap);
+  return (
+    <div>
+      <div id="mapid"></div>
+    </div>
+  );
+};
 
-        const circle = L.circle([this.props.location.latitude, this.props.location.longitude], {
-            color: 'blue',
-            fillColor: '#f03',
-            fillOpacity: 0.5,
-            radius: 100
-        }).addTo(myMap);
-
-        circle.bindPopup("Hey, that's me!")
-    }
-
-    render() {
-
-        return (
-            <div>
-                <div id="mapid"></div>
-            </div>
-        );
-    }
-}
+export default Map;
